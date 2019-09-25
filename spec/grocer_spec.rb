0988dc6242ddf043f44a6d2546fd0519f97bf0ec
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 19:05:59 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -146,7 +146,7 @@ describe "Grocer" do
         two_coupon_result = apply_coupons(consol_cart, [coupon, coupon])
 
         expect(two_coupon_result["AVOCADO"][:count]).to eq(1)
-        expect(two_coupon_result["AVOCADO W/COUPON"][:price]).to eq(2.50)
+        expect(two_coupon_result["AVOCADO W/COUPON"][:price]).to eq(5.00)
         expect(two_coupon_result["AVOCADO"][:price]).to eq(3.00)
         expect(two_coupon_result["AVOCADO W/COUPON"][:count]).to eq(4)
       end
