72bc840ffb58382dd682ad60dff108ebde9c287e
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 19:10:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,6 +44,13 @@ cart
 end
 
 def checkout(cart, coupons)
-  
+  total = 0 
+  new_cart = consolidate_cart(cart) 
+  coupon_cart = apply_coupons(new_cart, coupons) 
+  clearance_cart = apply_clearance(coupon_cart) 
+  clearance_cart.each do |item, attribute_hash| 
+    total += (attribute_hash[:price] * attribute_hash[:count])
+  end 
+total = (total * 0.9) if total > 100 total 
   
 end
