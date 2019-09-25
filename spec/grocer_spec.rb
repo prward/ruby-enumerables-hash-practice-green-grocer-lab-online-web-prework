2db10c606b21809dc14649350837633e10dccada
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 19:03:07 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -91,7 +91,7 @@ describe "Grocer" do
 
         expect(cheese_result["CHEESE"][:price]).to eq(6.50)
         expect(cheese_result["CHEESE"][:count]).to eq(2)
-        expect(cheese_result["CHEESE W/COUPON"][:price]).to eq(5.00)
+        expect(cheese_result["CHEESE W/COUPON"][:price]).to eq(15.00)
         expect(cheese_result["CHEESE W/COUPON"][:count]).to eq(3)
         expect(cheese_result["CHEESE W/COUPON"][:clearance]).to eq(false)
       end
