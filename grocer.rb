701acfc9fb256135955845be33387882b9f67f95
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 18:47:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,13 @@
 def consolidate_cart(cart)
- 
+ organized_cart = {}
+  count = 0
+  cart.each do |element|
+    element.each do |fruit, hash|
+      organized_cart[fruit] ||= hash
+      organized_cart[fruit][:count] ||= 0
+      organized_cart[fruit][:count] += 1
+    end
+  end
   
   
 end
