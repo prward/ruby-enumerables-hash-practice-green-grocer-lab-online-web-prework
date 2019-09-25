c6a98246b93299a2a0f88157483b85c8b990cf99
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 19:07:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,7 +33,15 @@ end
   
   
 def apply_clearance(cart)
-  
+  def apply_clearance(cart) 
+  cart.each do |item, attribute_hash| 
+    if attribute_hash[:clearance] == true 
+      attribute_hash[:price] = (attribute_hash[:price] *
+      0.8).round(2) 
+    end 
+  end 
+cart 
+end
  
 end
 
