51a9f2054d292445d1bcf19738590dca2e033516
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 18:46:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,17 +5,17 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  cart.each_with_object(Hash.new(0)) { |cart,counts| counts = 1 }
+  
   
   
 end
 
 def apply_clearance(cart)
   # code here
-  cart.each_with_object(Hash.new(0)) { |cart,counts| counts = 1 }
+ 
 end
 
 def checkout(cart, coupons)
   # code here
-  cart.each_with_object(Hash.new(0)) { |cart,counts| counts = 1 }
+  
 end
